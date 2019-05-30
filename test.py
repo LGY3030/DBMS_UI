a6898b0607db5524239c79967d85a030a7e39e55
GuanYu Lin <lgy3030@gmail.com>
2019-05-30 22:51:32 +0800

    Update

---
+++ test.py
@@ -0,0 +1,110 @@
+﻿# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'mainwindow.ui'
+#
+# Created by: PyQt5 UI code generator 5.12.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        MainWindow.setStyleSheet("background-color: rgb(225, 244, 255);")
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(80, 230, 631, 31))
+        self.pushButton.setStyleSheet("background-color: rgb(255, 255, 255);")
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton.clicked.connect(self.tes)
+        self.textEdit_2 = QtWidgets.QTextEdit(self.centralwidget)
+        self.textEdit_2.setGeometry(QtCore.QRect(330, 80, 381, 141))
+        self.textEdit_2.setStyleSheet("background-color: rgb(255, 254, 235);")
+        self.textEdit_2.setObjectName("textEdit_2")
+        self.textBrowser_2 = QtWidgets.QTextBrowser(self.centralwidget)
+        self.textBrowser_2.setGeometry(QtCore.QRect(80, 80, 231, 141))
+        self.textBrowser_2.setStyleSheet("background-color: rgb(169, 221, 255);\n"
+"font: 9pt \"微軟正黑體\";")
+        self.textBrowser_2.setObjectName("textBrowser_2")
+        self.widget_2 = QtWidgets.QWidget(self.centralwidget)
+        self.widget_2.setGeometry(QtCore.QRect(330, 20, 381, 51))
+        self.widget_2.setStyleSheet("background-color: rgb(255, 254, 235);")
+        self.widget_2.setObjectName("widget_2")
+        self.comboBox = QtWidgets.QComboBox(self.widget_2)
+        self.comboBox.setGeometry(QtCore.QRect(20, 10, 331, 31))
+        self.comboBox.setStyleSheet("background-color: rgb(255, 255, 255);")
+        self.comboBox.setObjectName("comboBox")
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.textBrowser = QtWidgets.QTextBrowser(self.centralwidget)
+        self.textBrowser.setGeometry(QtCore.QRect(80, 20, 231, 51))
+        self.textBrowser.setStyleSheet("background-color: rgb(169, 221, 255);\n"
+"font: 9pt \"微軟正黑體\";")
+        self.textBrowser.setObjectName("textBrowser")
+        self.widget = QtWidgets.QWidget(self.centralwidget)
+        self.widget.setGeometry(QtCore.QRect(50, 10, 691, 271))
+        self.widget.setStyleSheet("background-color: rgb(214, 235, 241);")
+        self.widget.setObjectName("widget")
+        self.textBrowser_3 = QtWidgets.QTextBrowser(self.centralwidget)
+        self.textBrowser_3.setGeometry(QtCore.QRect(50, 300, 691, 251))
+        self.textBrowser_3.setStyleSheet("background-color: rgb(60, 78, 143);")
+        self.textBrowser_3.setObjectName("textBrowser_3")
+        self.textBrowser_4 = QtWidgets.QTextBrowser(self.centralwidget)
+        self.textBrowser_4.setGeometry(QtCore.QRect(60, 310, 671, 231))
+        self.textBrowser_4.setStyleSheet("background-color: rgb(214, 235, 241);")
+        self.textBrowser_4.setObjectName("textBrowser_4")
+        self.widget.raise_()
+        self.pushButton.raise_()
+        self.textEdit_2.raise_()
+        self.textBrowser_2.raise_()
+        self.widget_2.raise_()
+        self.textBrowser.raise_()
+        self.textBrowser_3.raise_()
+        self.textBrowser_4.raise_()
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 25))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+    def tes(self):
+        print("test")
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Search"))
+        self.textBrowser_2.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'微軟正黑體\'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
+"<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
+"<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:\'標楷體\'; font-size:18pt; font-weight:600;\"><br /></p>\n"
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'標楷體\'; font-size:18pt; font-weight:600;\">查詢關鍵字</span></p></body></html>"))
+        self.comboBox.setItemText(0, _translate("MainWindow", "ID"))
+        self.comboBox.setItemText(1, _translate("MainWindow", "MYSQL"))
+        self.textBrowser.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'微軟正黑體\'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'標楷體\'; font-size:18pt; font-weight:600;\">查詢工具</span></p></body></html>"))
+
+
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
